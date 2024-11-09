c72520847e243e01c82f62dd1f7d8bccca397659
Gravy-Sasha <Sashe4ka1607@gmail.co>
2024-11-09 21:33:19 +0300

    Создана функция загрузки ссылки на изображение

--- Dogs.py
+++ Dogs.py
@@ -4,6 +4,8 @@ from PIL import Image, ImageTk
 from io import BytesIO
 from tkinter import messagebox as mb
 
+from Scripts.bottle import response
+
 
 def show_image():
     image_url = get_dog_image()
@@ -14,10 +16,21 @@ def show_image():
             img_data = BytesIO(response.content)
             img = Image.open(img_data)
             img.thumbnail((300,300))
+            img = ImageTk.PhotoImage(img)
             label.config(image=img)
             label.image = img
         except Exception as e:
-            mb.showerror('Ошибка!',f'Возникла ошибка: {e}')
+            mb.showerror('Ошибка!',f'Возникла ошибка при загрузке изображения: {e}')
+
+def get_dog_image():
+    try:
+        response = requests.get('https://dog.ceo/api/breeds/image/random')
+        response.raise_for_status()
+        data = response.json()
+        return data('message')
+    except Exception as e:
+        mb.showerror('Ошибка!',f'Возникла ошибка при запросе к api: {e}')
+        return None
 
 
 window = Tk()
